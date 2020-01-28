cb4797c3bfb061d4752d092156a8e9cae521e7ba
Roberto Esposito <roberto.esposito@unito.it>
2020-01-28 18:03:40 +0100

    Fixed problem in initcheckpoint

--- src/Checkpoint.jl
+++ src/Checkpoint.jl
@@ -17,15 +17,15 @@ function initcheckpoint(conf, data, path=".")
     try
         mkdir("$fullpath")
         write("$fullpath/conf.json", confstr)
+
+        @debug "Serializing initial data on path: $fullpath/data.jld"
+        serialize("$fullpath/data.jld", data)    
     catch err
         if err.errnum != 17
             @error "Error while creating $fullpath: $err"
             exit(err.errnum)
         end
     end
-
-    @debug "Serializing data on path: $fullpath/data.jld"
-    serialize("$fullpath/data.jld", data)
 end
 
 function checkpoint(conf, data; path=".")
