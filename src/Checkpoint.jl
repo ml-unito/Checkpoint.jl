f533a2d84f8567d9c9874d19d514959718f9ae38
Roberto Esposito <roberto.esposito@unito.it>
2020-01-29 16:02:24 +0100

    Changed API to support better initialization of checkpoints

--- src/Checkpoint.jl
+++ src/Checkpoint.jl
@@ -4,15 +4,33 @@ using Serialization
 using JSON
 using SHA
 
-export initcheckpoint, checkpoint, resume
+export checkpointpath, checkpoint, resume
 
-function __checkpointpath(conf, path)
+"""
+    checkpointpath(conf, path)
+
+    returns the complete path of the experiment identified by `conf`
+
+    # Arguments
+
+    * conf: the configuration of the experiment. Can be any data structure that
+        can be serialized to json
+    * path: path where the experiments need to be rooted
+"""
+function checkpointpath(conf, path)
     "$path/$(bytes2hex(sha256(json(conf))))"
 end
 
-function initcheckpoint(conf, data; path=".")
+
+"""
+    __initcheckpoint(conf, data; path)
+
+    Creates the directory where the checkpoints will be stored.
+    For internal use only.
+"""
+function __initcheckpoint(conf, data; path=".")
     confstr = json(conf)
-    fullpath = __checkpointpath(conf, path)
+    fullpath = checkpointpath(conf, path)
 
     try
         mkdir("$fullpath")
@@ -28,17 +46,47 @@ function initcheckpoint(conf, data; path=".")
     end
 end
 
+"""
+    Stores a new checkpoint for the experiment identified by `conf`.
+
+    The checkpoint will contain the data contained in the `data` parameter.
+
+    # Arguments
+
+    * conf: the configuration of the experiment. Can be any data structure that
+        can be serialized to json
+    * data: the data for the checkpoint. It should contain every piece of 
+        information needed to resume the eperiment.
+    * path: path where the experiments need to be rooted
+"""
 function checkpoint(conf, data; path=".")
-    fullpath = __checkpointpath(conf, path)
+    fullpath = checkpointpath(conf, path)
     @debug "Serializing data on path: $fullpath/data.jld"
     serialize("$fullpath/data.jld", data)
 end
 
-function resume(conf; path=".")
-    fullpath = __checkpointpath(conf, path)
+"""
+    Recovers the data stored in the checkpoint.
 
-    @debug "Deserializing data from paht: $fullpath/data.jld"
-    deserialize("$fullpath/data.jld")
+    # Arguments
+
+    * conf: the configuration of the experiment. Can be any data structure that
+        can be serialized to json
+    * init: the initialization values for the experiment. They should be such
+        that if the experiment were to be resumed from them, it would run
+        from the start.
+"""
+
+function resume(conf; init, path=".")
+    fullpath = checkpointpath(conf, path)
+
+    if isdir($fullpath)
+        @debug "Deserializing data from paht: $fullpath/data.jld"
+        return deserialize("$fullpath/data.jld")
+    else
+        __initcheckpoint(conf, init, path=path)
+        return init
+    end
 end
 
 end
